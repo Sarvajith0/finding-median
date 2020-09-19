3846bed9c4483c28f5063d8e7e8220ab6c07d16c
Sarvajith <65763658+Sarvajith0@users.noreply.github.com>
2020-09-19 19:00:08 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import statistics 
+data1 = [93,84,97,98,100,78,86,100,85,92,55,91,90,75,94,83,60,81,95,66] 
+  
+  
+# Printing median of the 
+# random data-set 
+print("Median of data-set is : % s "
+        % (statistics.median(data1)))
